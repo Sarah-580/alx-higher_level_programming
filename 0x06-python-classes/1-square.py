34afde22886c7dbdfbab0e71575d6d63b19a27f3
Sarah Wango'ondu <projectcode580@gmail.com>
2023-08-31 02:03:32 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Class Square is defined"""
 
 
 class Square:
-
+"""Class Square is represented"""
 
     def __init__(self, size):
+        """
+        New Square Initialized
 
-
-
+        size: size of square
+        """
         self.__size = size
