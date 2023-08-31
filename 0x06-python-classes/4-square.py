afce892d7068a1c0eab023fa2dc6eb4e9f044e6c
Sarah Wango'ondu <projectcode580@gmail.com>
2023-08-31 05:57:16 -0700

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
+"""Class Square is defined"""
+
 
 class Square:
+    """Class Square is Represented"""
+
     def __init__(self, size=0):
+        """
+        Initialize Class Square
+
+        size: size of new square
+        """
         self.size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        """set size of square"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-            def area(self):
-                return self.__size ** 2
+    def area(self):
+        """Return area of square"""
+        return (self.__size * self.__size)
