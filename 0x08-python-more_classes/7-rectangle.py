bde4c47280b176cc98c50c64d1ee3469b47f618a
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-09 05:22:59 -0700

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
+"""Define Rectangle class"""
+
 
 class Rectangle:
+    """
+    Rectangle class representation
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances
+        print_symbol (any): The symbol used for string representation
+    """
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """
+        Initialize a new Rectangle
 
+        Args:
+            width (int): width of Rectangle
+            height (int): height of Rectangle
+        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """get/set width of the Rectangle"""
         return self.__width
 
     @width.setter
@@ -25,6 +41,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """get/set height of the Rectangle"""
         return self.__height
 
     @height.setter
@@ -36,22 +53,27 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return area of the Rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return perimeter of the Rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-       if self.__width == 0 or self.__height == 0:
-           return ""
-       return "\n".join([str(self.print_symbol) * self.__width
-           for _ in range(self.__height)])
+        """Return presentation of the Rectangle with the # character"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.__width
+            for _ in range(self.__height)])
 
     def __repr__(self):
-       return 'Rectangle({}, {})'.format(self.__width, self.__height
+        """Return printable presentation of the Rectangle"""
+        return 'Rectangle({}, {})'.format(self.__width, self.__height
 
     def __del__(self):
-       print("Bye rectangle...")
-       Rectangle.number_of_instances -= 1
+        """Prints a message for every deletion of a Rectangle"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
