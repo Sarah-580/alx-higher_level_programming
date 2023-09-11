596228f0e330b8098d44c5f2038ed763438749df
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-11 07:07:53 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
+"""Defines a matrix division function"""
+
+
 def matrix_divided(matrix, div):
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    """
+    Divide all elements of a matrix
+
+    Args:
+        matrix (list): A list of lists of ints or floats
+        div (int/float): The divisor
+
+    Raises:
+        TypeError: If the matrix contains non-numbers
+        TypeError: If the matrix contains rows of different sizes
+        TypeError: If div is not an int or float
+        ZeroDivisionError: If div is 0
+
+    Returns:
+        A new matrix representing the result of the division
+    """
+
     if not matrix:
         raise TypeError(errorMessage)
     if not isinstance(matrix, list):
         raise TypeError(errorMessage)
+
     for lists in matrix:
         if not isinstance(lists, list):
             raise TypeError(errorMessage)
         for item in lists:
             if not isinstance(item, int) and not isinstance(item, float):
                 raise TypeError(errorMessage)
+
     for lists in matrix:
         if len(lists) == 0:
             raise TypeError(errorMessage)
@@ -20,4 +41,5 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         if div == 0:
             raise ZeroDivisionError("division by zero")
+
         return [[round(item / div, 2) for item in lists] for lists in matrix]
