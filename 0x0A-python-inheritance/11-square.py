e18ceff64873096cb6155e958fde54775a5add36
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-12 20:52:41 -0700

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,7 +9,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize a anew square"""
-
-        self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
+
+        def __str__(self):
+            """return strig representation"""
+            return "[Square] {}/{}".format(self._Rectangle__width,
+                    self._Rectangle__height)
