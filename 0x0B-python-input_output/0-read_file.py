1e95f6a99e7406e521d66fee32ec7dd24e732e78
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-14 18:41:31 -0700

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define a text file-reading function"""
+
+
 def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout"""
     with open(filename, 'r') as f:
         for li in f:
             print(li, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Defines a file-writing function"""
 
 
 def write_file(filename="", text=""):
@@ -8,4 +8,4 @@ def write_file(filename="", text=""):
     returns the number of characters written
     """
     with open(filename, mode="w", encoding="utf-8") as f:
-        return(f.write(text))
+        return (f.write(text))
