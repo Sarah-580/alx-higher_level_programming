9e876e27ae645f08da19f0f14469800316fa9064
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-19 07:21:04 -0700

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,4 +56,5 @@ class Rectangle:
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height -1:
                 rect.append("\n")
+
         return ("".join(rect))
